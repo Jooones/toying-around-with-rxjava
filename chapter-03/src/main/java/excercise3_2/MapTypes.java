33afc6f46652eb0bee24bd73ae64f7efd9f3d5e3
Steven Op de beeck <steven@opdebeeck.org>
2018-02-04 16:16:11 +0100

    stevenodb: fixes

--- chapter-03/src/main/java/excercise3_2/MapTypes.java
+++ chapter-03/src/main/java/excercise3_2/MapTypes.java
@@ -1,54 +1,52 @@
 package excercise3_2;
 
-
 import rx.Observable;
 
 import static java.util.concurrent.ThreadLocalRandom.current;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static rx.Observable.just;
 
 public class MapTypes {
 
-
-    private static final int STREAM_SIZE = 5;
-    private static final int DELAY = 2000;
+    private static final int TWO_THOUSAND = 2000;
 
     /**
      * In which order will the items emitted by getObservable() be printed in each of the 3 cases?
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Output with map: ");
         getObservable()
-                .map(integer -> just(integer + 1).delay(current().nextInt(0, DELAY), MILLISECONDS))
+                .map(integer -> just(integer + 1).delay(current().nextInt(0, TWO_THOUSAND), MILLISECONDS))
                 .map(integerObservable -> integerObservable.toBlocking().first())
-                .toBlocking()
                 .subscribe(System.out::println);
+
+        SECONDS.sleep(5);
 
         /*########################################*/
 
         System.out.println("Output with flatmap: ");
         getObservable()
                 .flatMap(integer -> just(integer+1)
-                        .delay(current().nextInt(0, DELAY), MILLISECONDS))
-                .toBlocking()
+                        .delay(current().nextInt(0, TWO_THOUSAND), MILLISECONDS))
                 .subscribe(System.out::println);
+
+        SECONDS.sleep(5);
 
         /*########################################*/
 
         System.out.println("Output with concatmap: ");
         getObservable()
-                .concatMap(integer -> just(integer+1)
-                        .delay(current().nextInt(0, DELAY), MILLISECONDS))
-                .toBlocking()
+                .concatMap(integer -> just(integer+1).delay(current().nextInt(0, TWO_THOUSAND), MILLISECONDS))
                 .subscribe(System.out::println);
+
+        SECONDS.sleep(10);
 
         /*########################################*/
 
     }
 
-    private static Observable<Integer> getObservable(){
-        return Observable.range(0, STREAM_SIZE);
+    private static Observable<Integer> getObservable() {
+        return Observable.range(0, 5);
     }
-
-
 }
